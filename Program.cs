09d083f942d89d209dc1f8cf24065954e3b4ce8c
carlos_costa <carloscosta.pen@gmail.com>
2026-02-26 11:59:58 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathLibrary;
+
+namespace Library_Referencing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMath Math = new clsMath();
+            Console.WriteLine(Math.Sum(10, 20));
+            Console.ReadKey();
+
+        }
+    }
+}
